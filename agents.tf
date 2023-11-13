209626f40a662936bad4ed172827e7b23f8658f4
Chris Trauco <chris@trau.co>
2023-11-12 23:50:35 -0500

    entity types need defining before all other things in agent design best practices

--- agents.tf
+++ agents.tf
@@ -10,9 +10,9 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
 
-resource "google_dialogflow_cx_agent" "agent" {
+
+resource "google_dialogflow_cx_agent" "bc-records-agent" {
   display_name          = "bc-records-agent"
   location              = var.region
   default_language_code = "en"
