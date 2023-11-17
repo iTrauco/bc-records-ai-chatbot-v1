68ab044708cf54fe5e5715a3426e245c5c644f9c
Chris Trauco <chris@trau.co>
2023-11-16 23:25:25 -0500

    entity types added

--- entity-types.tf
+++ entity-types.tf
@@ -1,6 +1,6 @@
-resource "google_dialogflow_cx_entity_type" "artists" {
+resource "google_dialogflow_cx_entity_type" "artist" {
   parent       = google_dialogflow_cx_agent.agent.id
-  display_name = "Artists"
+  display_name = "Artist"
   kind         = "KIND_MAP"
   entities {
     value = "The Google Dolls"
@@ -18,13 +18,13 @@ resource "google_dialogflow_cx_entity_type" "artists" {
   redact = true
 } 
 
-resource "google_dialogflow_cx_entity_type" "artists_aliases" {
+resource "google_dialogflow_cx_entity_type" "artist_aliases" {
   parent       = google_dialogflow_cx_agent.agent.id
   display_name = "Artist_Aliases"
   kind         = "KIND_MAP"
   entities {
-    value = google_dialogflow_cx_entity_type.artists.display_name
-    synonyms = ["musician","bands", "singers"]
+    value = google_dialogflow_cx_entity_type.artist.display_name
+    synonyms = ["musicians","bands", "singers", "groups"]
   }
   enable_fuzzy_extraction = false
 } 
@@ -55,62 +55,64 @@ resource "google_dialogflow_cx_entity_type" "merch" {
   }
 } 
 
-# resource "google_dialogflow_cx_entity_type" "basic_entity_album" {
-#   parent       = google_dialogflow_cx_agent.agent.id
-#   display_name = "Album"
-#   kind         = "KIND_MAP"
-#   entities {
-#     value = "Live"
-#   }
-#   entities {
-#     value = "Greatest Hits"
-#     synonyms = ["Hits"]
-#   }
-#   enable_fuzzy_extraction = true
-#   redact = true
-# } 
+resource "google_dialogflow_cx_entity_type" "album" {
+  parent       = google_dialogflow_cx_agent.agent.id
+  display_name = "Album"
+  kind         = "KIND_MAP"
+  entities {
+    value = "Live"
+    synonyms = ["Live"]
+  }
+  entities {
+    value = "Greatest Hits"
+    synonyms = ["Hits"]
+  }
+  enable_fuzzy_extraction = true
+  redact = true
+} 
 
-# resource "google_dialogflow_cx_entity_type" "basic_entity_shirtsize" {
-#   parent       = google_dialogflow_cx_agent.agent.id
-#   display_name = "ShirtSize"
-#   kind         = "KIND_MAP"
-#   entities {
-#     value = "XS"
-#     synonyms = ["Extra Small"]
-#   }
-#   entities {
-#     value = "S"
-#     synonyms = ["Small"]
-#   }
+resource "google_dialogflow_cx_entity_type" "shirtsize" {
+  parent       = google_dialogflow_cx_agent.agent.id
+  display_name = "ShirtSize"
+  kind         = "KIND_MAP"
+  entities {
+    value = "XS"
+    synonyms = ["Extra Small"]
+  }
+  entities {
+    value = "S"
+    synonyms = ["Small"]
+  }
 
-#   entities {
-#     value = "M"
-#     synonyms = ["Medium"]
-#   }
-#   entities {
-#     value = "L"
-#     synonyms = ["Large"]
-#   }
-#   entities {
-#     value = "XL"
-#     synonyms = ["Extra Large"]
-#   }
-#   entities {
-#     value = "2XL"
-#     synonyms = ["Extra Extra Large"]
-#   }
-#   enable_fuzzy_extraction = true 
-#   redact = true
-# } 
+  entities {
+    value = "M"
+    synonyms = ["Medium"]
+  }
+  entities {
+    value = "L"
+    synonyms = ["Large"]
+  }
+  entities {
+    value = "XL"
+    synonyms = ["Extra Large"]
+  }
+  entities {
+    value = "2XL"
+    synonyms = ["Extra Extra Large"]
+  }
+  enable_fuzzy_extraction = true 
+  redact = true
+} 
 
 
 
-# resource "google_dialogflow_cx_entity_type" "basic_entity_order_number" {
-#   parent       = google_dialogflow_cx_agent.agent.id
-#   display_name = "OrderNumber"
-#   kind         = "KIND_REGEXP"
-#   entities {
-#     value = "[A-Z]{4}[0-9]{3}"
-#   }
-#   enable_fuzzy_extraction = false 
-# } 
+resource "google_dialogflow_cx_entity_type" "order_number" {
+  parent       = google_dialogflow_cx_agent.agent.id
+  display_name = "OrderNumber"
+  kind         = "KIND_REGEXP"
+  entities {
+    value = "[A-Z]{4}[0-9]{3}"
+    synonyms = ["[A-Z]{4}[0-9]{3}"]
+  }
+  enable_fuzzy_extraction = false 
+} 
